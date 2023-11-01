2bbec79d6c30390cf9aff67230c7d16aa6f0aa17
sumeyraltas <sumeyraltas@gmail.com>
2023-11-01 21:15:59 +0300

    Initial Commit

---
+++ My First Project/My_First_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_First_ProjectApp.swift
+//  My First Project
+//
+//  Created by Sumeyra Altas on 1.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct My_First_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
