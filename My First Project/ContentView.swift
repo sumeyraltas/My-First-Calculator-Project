3bccab371480d3dfa80508e434515e744d6b3a1d
sumeyraltas <sumeyraltas@gmail.com>
2023-11-02 13:52:22 +0300

    single page calculator app should correct horizontal orientation.

--- My First Project/ContentView.swift
+++ My First Project/ContentView.swift
@@ -4,21 +4,127 @@
 //
 //  Created by Sumeyra Altas on 1.11.2023.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var displayText = "0"
+    @State private var firstNumber = 0.0
+    @State private var secondNumber = 0.0
+    @State private var currentOperation = ""
+    @State private var isTyping = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text(displayText)
+                .font(.system(size: 80))
+                .padding()
+                
+            Spacer()
+            
+            HStack {
+                Button("1") {
+                    handleNumberButton(1)
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+                Button("2") {
+                    handleNumberButton(2)
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+                Button("3") {
+                    handleNumberButton(3)
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+                Button("+") {
+                    handleOperationButton("+")
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+            }
+            HStack {
+                Button("4") {
+                    handleNumberButton(4)
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+                Button("5") {
+                    handleNumberButton(5)
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+                Button("6") {
+                    handleNumberButton(6)
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+                Button("-") {
+                    handleOperationButton("-")
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+            }
+            HStack {
+                Button("7") {
+                    handleNumberButton(7)
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+                Button("8") {
+                    handleNumberButton(8)
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+                Button("9") {
+                    handleNumberButton(9)
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+                Button("*") {
+                    handleOperationButton("*")
+
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+            }
+            HStack {
+                Button("C") {
+                    clear()
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+                Button("0") {
+                    handleNumberButton(0)
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+                Button("=") {
+                    calculateResult()
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+                Button("/") {
+                    handleOperationButton("/")
+                }.font(.system(size: 68)).frame(width: 80, height: 80)
+            }
         }
-        .padding()
+    }
+    
+    func handleNumberButton(_ number: Int) {
+        if isTyping {
+            displayText += "\(number)"
+        } else {
+            displayText = "\(number)"
+        }
+        isTyping = true
+    }
+    
+    func handleOperationButton(_ operation: String) {
+        firstNumber = Double(displayText) ?? 0
+        currentOperation = operation
+        isTyping = false
+        displayText = currentOperation    }
+    
+    func calculateResult() {
+        secondNumber = Double(displayText) ?? 0
+        
+        switch currentOperation {
+        case "+":
+            displayText = "\(firstNumber + secondNumber)"
+        case "-":
+            displayText = "\(firstNumber - secondNumber)"
+        case "*":
+            displayText = "\(firstNumber * secondNumber)"
+        case "/":
+            displayText = "\(firstNumber / secondNumber)"
+            
+        default:
+            break
+        }
+        
+    }
+    
+    func clear() {
+        displayText = "0"
+        firstNumber = 0
+        secondNumber = 0
+        currentOperation = ""
+        isTyping = false
     }
 }
-
+    
 #Preview {
     ContentView()
 }
--- My First Project/ViewController.swift
+++ My First Project/ViewController.swift
@@ -6,3 +6,57 @@
 //
 
 import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var firstNumber: Double?
+    var secondNumber: Double?
+    var currentOperation: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+        if let digit = sender.currentTitle, let currentText = resultLabel.text {
+            resultLabel.text = currentText + digit
+        }
+    }
+    
+    @IBAction func operationButtonPressed(_ sender: UIButton) {
+        if let operation = sender.currentTitle, let currentText = resultLabel.text {
+            firstNumber = Double(currentText)
+            currentOperation = operation
+            resultLabel.text = ""
+        }
+    }
+    
+    @IBAction func equalButtonPressed(_ sender: UIButton) {
+        if let firstNum = firstNumber, let currentText = resultLabel.text {
+            if let secondNum = Double(currentText) {
+                switch currentOperation {
+                case "+":
+                    resultLabel.text = "\(firstNum + secondNum)"
+                case "-":
+                    resultLabel.text = "\(firstNum - secondNum)"
+                case "×":
+                    resultLabel.text = "\(firstNum * secondNum)"
+                case "÷":
+                    resultLabel.text = "\(firstNum / secondNum)"
+                default:
+                    break
+                }
+            }
+        }
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: UIButton) {
+        firstNumber = nil
+        secondNumber = nil
+        currentOperation = nil
+        resultLabel.text = ""
+    }
+}
