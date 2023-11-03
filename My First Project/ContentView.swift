2b02ff9936d7b2a88083429bc787300f121e67d4
sumeyraltas <sumeyraltas@gmail.com>
2023-11-03 16:07:06 +0300

    Rotation features completed, gray round background added to buttons (feature trial).

--- My First Project/ContentView.swift
+++ My First Project/ContentView.swift
@@ -14,72 +14,150 @@ struct ContentView: View {
     @State private var isTyping = false
     
     var body: some View {
-        VStack {
-            Spacer()
-            Text(displayText)
-                .font(.system(size: 80))
-                .padding()
-                
-            Spacer()
-            
+        GeometryReader { geometry in
             HStack {
-                Button("1") {
-                    handleNumberButton(1)
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
-                Button("2") {
-                    handleNumberButton(2)
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
-                Button("3") {
-                    handleNumberButton(3)
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
-                Button("+") {
-                    handleOperationButton("+")
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
-            }
-            HStack {
-                Button("4") {
-                    handleNumberButton(4)
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
-                Button("5") {
-                    handleNumberButton(5)
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
-                Button("6") {
-                    handleNumberButton(6)
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
-                Button("-") {
-                    handleOperationButton("-")
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
-            }
-            HStack {
-                Button("7") {
-                    handleNumberButton(7)
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
-                Button("8") {
-                    handleNumberButton(8)
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
-                Button("9") {
-                    handleNumberButton(9)
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
-                Button("*") {
-                    handleOperationButton("*")
+                if geometry.size.width < geometry.size.height {
+                    VStack {
+                        Spacer()
+                        Text(displayText)
+                            .font(.system(size: 80))
+                            .padding()
+                        Spacer()
+                        HStack {
+                            Spacer()
+                            Button("1") {
+                                handleNumberButton(1)
+                            }.font(.system(size: 68)).frame(width: 80, height: 80) .background(Circle().fill(Color.gray.opacity(0.3)))
 
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
+                            Button("2") {
+                                handleNumberButton(2)
+                            }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                            Button("3") {
+                                handleNumberButton(3)
+                            }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                            Button("+") {
+                                handleOperationButton("+")
+                            }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                            Spacer()
+                        }
+                        HStack {
+                            Button("4") {
+                                handleNumberButton(4)
+                            }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                            Button("5") {
+                                handleNumberButton(5)
+                            }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                            Button("6") {
+                                handleNumberButton(6)
+                            }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                            Button("-") {
+                                handleOperationButton("-")
+                            }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                            
+                        }
+                        HStack {
+                            Button("7") {
+                                handleNumberButton(7)
+                            }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                            Button("8") {
+                                handleNumberButton(8)
+                            }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                            Button("9") {
+                                handleNumberButton(9)
+                            }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                            Button("*") {
+                                handleOperationButton("*")
+                                
+                            }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                        }
+                        HStack {
+                            Button("C") {
+                                clear()
+                            }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                            Button("0") {
+                                handleNumberButton(0)
+                            }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                            Button("=") {
+                                calculateResult()
+                            }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                            Button("/") {
+                                handleOperationButton("/")
+                            }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                        }
+                
+                    }
+                }
+                    else {
+                        HStack {
+                           Spacer()
+                            Text(displayText)
+                                .font(.system(size: 80))
+                                .padding()
+                            Spacer()
+                            VStack {
+                                Spacer()
+                                Button("1") {
+                                    handleNumberButton(1)
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                                Button("4") {
+                                    handleNumberButton(4)
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                                Button("7") {
+                                    handleNumberButton(7)
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                                Button("C") {
+                                    clear()
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                                
+                            }
+                            VStack {
+                                Spacer()
+                                Button("2") {
+                                    handleNumberButton(2)
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                                Button("5") {
+                                    handleNumberButton(5)
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                                Button("8") {
+                                    handleNumberButton(8)
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                                Button("0") {
+                                    handleNumberButton(0)
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                            }
+                            VStack {
+                                Spacer()
+                                Button("3") {
+                                    handleNumberButton(3)
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                                Button("6") {
+                                    handleNumberButton(6)
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                                Button("9") {
+                                    handleNumberButton(9)
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                                Button("=") {
+                                    calculateResult()
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))                             }
+                            VStack {
+                                Spacer()
+                                Button("+") {
+                                    handleOperationButton("+")
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                                Button("-") {
+                                    handleOperationButton("-")
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                                Button("*") {
+                                    handleOperationButton("*")
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                                Button("/") {
+                                    handleOperationButton("/")
+                                }.font(.system(size: 68)).frame(width: 80, height: 80).background(Circle().fill(Color.gray.opacity(0.3)))
+                            }
+                        }
+                    }
+                }
             }
-            HStack {
-                Button("C") {
-                    clear()
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
-                Button("0") {
-                    handleNumberButton(0)
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
-                Button("=") {
-                    calculateResult()
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
-                Button("/") {
-                    handleOperationButton("/")
-                }.font(.system(size: 68)).frame(width: 80, height: 80)
-            }
-        }
     }
     
     func handleNumberButton(_ number: Int) {
@@ -95,11 +173,11 @@ struct ContentView: View {
         firstNumber = Double(displayText) ?? 0
         currentOperation = operation
         isTyping = false
-        displayText = currentOperation    }
+        displayText = currentOperation   
+    }
     
     func calculateResult() {
         secondNumber = Double(displayText) ?? 0
-        
         switch currentOperation {
         case "+":
             displayText = "\(firstNumber + secondNumber)"
